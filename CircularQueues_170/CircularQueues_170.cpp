b8f554e2124b9f8fb31f9442bfec2d9795b0a465
kaniadhea <kaniadhea7@gmail.com>
2023-06-23 09:14:49 +0700

    void

--- CircularQueues_170/CircularQueues_170.cpp
+++ CircularQueues_170/CircularQueues_170.cpp
@@ -4,4 +4,17 @@ using namespace std;
 class Queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+	}
 };
