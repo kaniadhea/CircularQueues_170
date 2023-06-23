aeac94a8e79ac56da98d6b7b13b3a464ada8961d
kaniadhea <kaniadhea7@gmail.com>
2023-06-23 11:02:53 +0700

    front

--- CircularQueues_170/CircularQueues_170.cpp
+++ CircularQueues_170/CircularQueues_170.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Queues {
-	int FRONT, REAR, max = 5;
+	int FRONT, REAR, n = 5;
 	int queue_array[5];
 
 public:
@@ -18,7 +18,7 @@ public:
 		cout << endl;
 
 		//cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+		if ((FRONT == 0 && REAR == n - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
@@ -30,7 +30,7 @@ public:
 		}
 		else {
 			//jika REAR berada diposisi terakhir array, kembali ke awal array
-			if (REAR == max - 1)
+			if (REAR == n - 1)
 				REAR = 0;
 			else
 				REAR = REAR + 1;
@@ -53,7 +53,7 @@ public:
 		}
 		else {
 			//jika elemen yang dihapus berada diposisi terakhir array, kembali ke awal array
-			if (FRONT == max - 1)
+			if (FRONT == n - 1)
 				FRONT = 0;
 			else
 				FRONT = FRONT + 1;
@@ -77,6 +77,13 @@ public:
 			while (FRONT_position <= REAR_position) {
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= n - 1) {
+
 			}
 		}
 	}
