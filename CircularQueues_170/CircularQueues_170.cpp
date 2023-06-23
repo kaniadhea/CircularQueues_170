c2707ad5ee273b36edbe524de2822ddfa0e0a850
kaniadhea <kaniadhea7@gmail.com>
2023-06-23 10:57:00 +0700

    elemen queue

--- CircularQueues_170/CircularQueues_170.cpp
+++ CircularQueues_170/CircularQueues_170.cpp
@@ -61,6 +61,16 @@ public:
 	}
 
 	void display() {
-		int FRONT 
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong 
+		if (FRONT == -1) {
+			cout << "Queue is empty\n ";
+			return;
+		}
+
+		cout << "\nElements in the queue are...\n";
+
 	}
 };
